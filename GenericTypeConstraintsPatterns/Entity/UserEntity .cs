52be30d6e6070e2f836741f162e537dfe44a0155
sheephuman <shin.citysheep@gmail.com>
2026-05-06 20:22:24 +0900

    引数無しｺﾝｽﾄﾗｸﾀの追加 delegate注入 class制約追加

--- GenericTypeConstraintsPatterns/Entity/UserEntity .cs
+++ GenericTypeConstraintsPatterns/Entity/UserEntity .cs
@@ -9,8 +9,23 @@ using System.Text;
 namespace GenericTypeConstraintsPatterns.Entity
 {
 
-    public class UserEntity : ICsvReadable<LogEntity>, IListItem
+    public class UserEntity : ICsvReadable<UserEntity>, IListItem
     {
+
+        /// <summary>
+        /// 'UserEntity' は、ジェネリック型またはメソッド 'ListViewModel<TEntity>' 内でパラメーター 'TEntity' として使用するために、パブリック パラメーターなしのコンストラクターを持つ非抽象型でなければなりません　　対応
+        /// 引数なしのコンストラクターを追加することで、ListViewModel<UserEntity> の制約を満たすことができます。
+        /// </summary>
+        public UserEntity() { }  // 追加
+
+
+        public UserEntity(int id, string displayName, string affiliation)
+        {
+            Id = id;
+            DisplayName = displayName;
+            this.affiliation = affiliation;
+        }
+
         public int Id { get; set; }
         public string DisplayName { get; set; } = string.Empty;
         public string affiliation { get; set; } = string.Empty;
--- GenericTypeConstraintsPatterns/Loader/CSVLoader.cs
+++ GenericTypeConstraintsPatterns/Loader/CSVLoader.cs
@@ -4,21 +4,33 @@ using GenericTypeConstraintsPatterns.Repository;
 
 namespace GenericTypeConstraintsPatterns.Loader
 {
+
+    /// <summary>
+    /// 型 'TEntity' は、ジェネリック型のパラメーター 'TEntity'、またはメソッド 'CsvRepository<TEntity>' として使用するために、参照型でなければなりません　対応　制約の食い違いが原因
+    /// →　where  TEntity : class, ICsvReadable<TEntity>
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
     public sealed class CsvLoader<TEntity> : ICsvLoader<TEntity>
-        where TEntity : ICsvReadable<TEntity>
+        where  TEntity : class, ICsvReadable<TEntity>
     {
-        public IEnumerable<UserEntity> CsvLoad(string filePath)
+        private readonly Func<string[], TEntity> _factory;
+
+        public CsvLoader(Func<string[], TEntity> factory)
         {
-            var repository = new CsvRepository<UserEntity>(filePath, line =>
-            {
-                var userEntity = new UserEntity
-                {
-                    Id = int.Parse(line[0]),
-                    DisplayName = line[1],
-                    affiliation = line[2]
-                };
-                return userEntity;
-            });
+            _factory = factory;
+        }
+
+        public IEnumerable<TEntity> CsvLoad(string filePath)
+        {
+            ///
+            ////
+            ///型 'TEntity' は、ジェネリック型のパラメーター 'TEntity'、またはメソッド 'CsvRepository<TEntity>' として使用するために、参照型でなければなりません
+            ///→　CsvLoader<TEntity> の where に class を追加
+            /// 
+            ////
+
+
+            var repository = new CsvRepository<TEntity>(filePath, _factory);
             return repository.LoadFromCsv();
         }
     }
